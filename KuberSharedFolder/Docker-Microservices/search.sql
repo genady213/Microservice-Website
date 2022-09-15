f35c97220dc3d71535cb24909fdefab736a14935
genady213 <109979799+genady213@users.noreply.github.com>
2022-09-15 00:51:40 -0400

    The actual code

---
+++ KuberSharedFolder/Docker-Microservices/search.sql
@@ -0,0 +1,109 @@
+-- MySQL dump 10.18  Distrib 10.3.27-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: search_LBS
+-- ------------------------------------------------------
+-- Server version	10.3.27-MariaDB-0+deb10u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `AUTHOR`
+--
+
+DROP TABLE IF EXISTS `AUTHOR`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `AUTHOR` (
+  `firstName` varchar(250) NOT NULL,
+  `lastName` varchar(250) NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `AUTHOR`
+--
+
+LOCK TABLES `AUTHOR` WRITE;
+/*!40000 ALTER TABLE `AUTHOR` DISABLE KEYS */;
+INSERT INTO `AUTHOR` VALUES ('Lucy ','Montgomery ',1),('J. K. ','Rowling',2);
+/*!40000 ALTER TABLE `AUTHOR` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `BOOK`
+--
+
+DROP TABLE IF EXISTS `BOOK`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `BOOK` (
+  `isbn` varchar(20) NOT NULL,
+  `title` varchar(250) NOT NULL,
+  `barcode` varchar(20) NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `isbn` (`isbn`),
+  UNIQUE KEY `barcode` (`barcode`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `BOOK`
+--
+
+LOCK TABLES `BOOK` WRITE;
+/*!40000 ALTER TABLE `BOOK` DISABLE KEYS */;
+INSERT INTO `BOOK` VALUES ('111-1111-1111','anne of green gables','666-666-6666',1),('222-2222-2222','Harry Potter','444-444-4444',2);
+/*!40000 ALTER TABLE `BOOK` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `BOOK_AUTHOR`
+--
+
+DROP TABLE IF EXISTS `BOOK_AUTHOR`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `BOOK_AUTHOR` (
+  `bookid` int(10) unsigned NOT NULL,
+  `authorid` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`bookid`,`authorid`),
+  KEY `FK_authorid` (`authorid`),
+  CONSTRAINT `FK_authorid` FOREIGN KEY (`authorid`) REFERENCES `AUTHOR` (`id`),
+  CONSTRAINT `FK_bookid` FOREIGN KEY (`bookid`) REFERENCES `BOOK` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `BOOK_AUTHOR`
+--
+
+LOCK TABLES `BOOK_AUTHOR` WRITE;
+/*!40000 ALTER TABLE `BOOK_AUTHOR` DISABLE KEYS */;
+INSERT INTO `BOOK_AUTHOR` VALUES (1,1),(2,2);
+/*!40000 ALTER TABLE `BOOK_AUTHOR` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-06  4:06:44
